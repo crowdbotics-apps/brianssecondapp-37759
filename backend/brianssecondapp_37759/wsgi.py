aaee771cd82a803cbff69f4d18c8e27035cae91d
Crowdbotics <team@crowdbotics.com>
2022-11-30 18:33:48 +0000

    Initial commit.

---
+++ backend/brianssecondapp_37759/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for brianssecondapp_37759 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brianssecondapp_37759.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "brianssecondapp_37759",
+  url: "https://brianssecondapp_37759.botics.co",
+  api: "https://brianssecondapp_37759.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
